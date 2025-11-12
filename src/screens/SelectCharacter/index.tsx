72e8a565b5f8947994d35dcfdd171c81ae7dfcb7
patrikmagalhaes <patrikmagalhaes@icloud.com>
2025-11-12 11:06:49 -0300

    feat(screen character) add button

--- src/screens/SelectCharacter/index.tsx
+++ src/screens/SelectCharacter/index.tsx
@@ -7,9 +7,10 @@ function SelectCharacter() {
         <>
             <ContainerCharacter>
                 <OrnamentImage style={{transform: "rotate(180deg)", top:"0"}} src="./src/assets/teste/ornament.png" />
-                <h2>Com quem deseja conversar?</h2>
+                <h2 style={{fontSize:"2rem", textAlign: "center", marginBottom:"10%"}}>Com quem deseja conversar?</h2>
                 <Carousel />
                 <OrnamentImage style={{bottom:"0"}} src="./src/assets/teste/ornament.png" />
+                <button>Conversar</button>
             </ContainerCharacter>
 
         </>
